b5cbf4c61ee31e101b7c73bc141818a09543a53b
David Martos <dmartos@topfarma.es>
2023-02-15 13:43:56 +0100

    Fix word

--- utils/middleware.js
+++ utils/middleware.js
@@ -6,7 +6,7 @@ const unknownEndpoint = (request, response) => {
 
 const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+    return response.status(400).send({ error: 'malformed id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   } else if (error.name === 'JsonWebTokenError') {
